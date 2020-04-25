142ef00d14a57d335b1657bfa50ff616442f86bd
lxl <1827894800@qq.com>
2020-04-25 14:59:29 +0800

    first

---
+++ cust/src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="about" class="container">
+      <h1 class="page-header">感谢！</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+
+</style>
